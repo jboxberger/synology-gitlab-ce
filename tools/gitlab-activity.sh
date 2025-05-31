ddbf3791b5cb0731517f017c071f09e3dfcc90e5
jboxberger <1545845+jboxberger@users.noreply.github.com>
2025-05-31 22:13:02 +0200

    add DSM 7.2 compatibillity to gitlab activity tracker

--- tools/gitlab-activity.sh
+++ tools/gitlab-activity.sh
@@ -2,8 +2,13 @@
 
 # ls --sort=time | grep production_json.log | head -2
 
-GITLAB_CONTAINER_NAME="synology-gitlab-ce"
 docker_bin=$(which docker)
+GITLAB_INACTIVITY_SECONDS=3600
+GITLAB_CONTAINER_NAME="synology-gitlab-ce"
+CONTAINER_SERVICE="Docker"
+if [ -d "/var/packages/ContainerManager" ]; then
+  CONTAINER_SERVICE="ContainerManager"
+fi
 
 tmp_file="/tmp/$GITLAB_CONTAINER_NAME"
 last_log="/tmp/$GITLAB_CONTAINER_NAME.log"
@@ -37,11 +42,11 @@ if ! [ -z "$docker_bin" ]; then
 
     echo $last_activity_unix $current_unix $diffSeconds
 
-    if [ $diffSeconds -gt 3600 ]; then
+    if [ $diffSeconds -gt $GITLAB_INACTIVITY_SECONDS ]; then
       echo "inactivity treshold reached, shutting down"
       #synowebapi --exec api=SYNO.Docker.Container version=1 method=stop name="$GITLAB_CONTAINER_NAME" 2> /dev/null
       synowebapi --exec api=SYNO.Core.Package.Control id="$GITLAB_CONTAINER_NAME" method=stop version=1 2> /dev/null
-      synowebapi --exec api=SYNO.Core.Package.Control id=Docker method=stop version=1 2> /dev/null
+      synowebapi --exec api=SYNO.Core.Package.Control id="$CONTAINER_SERVICE" method=stop version=1 2> /dev/null
     else
       echo "do nothing"
     fi
