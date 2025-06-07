82a7ec2c4fc86d877c7af166274681993d865eb2
jboxberger <1545845+jboxberger@users.noreply.github.com>
2025-06-08 00:56:37 +0200

    remove unneeded config from install routine

--- src/WIZARD_UIFILES/install_uifile.sh
+++ src/WIZARD_UIFILES/install_uifile.sh
@@ -37,66 +37,6 @@ mem_total_check_fail="A minimum of {1} RAM is required to install or update <a h
 mem_total_check_warning="A minimum of {1} RAM is required to install or update GitLab. Insufficient memory may cause unexpected errors when you run GitLab, so you are recommended to expand the memory of your Synology NAS. <a href=\"{2}\" target=\"_blank\">Learn more</a>."
 additional_memory_required="Insufficient memory"
 
-hostname_label="Hostname/Domain"
-hostname_desc="The hostname of your synology server, also used in emails ("localhost" or "127.0.0.1" will not work)"
-
-ssh_port_label="SSH port number"
-ssh_port_desc="Please enter the SSH port number."
-
-http_port_label="HTTP port number"
-http_port_desc="Please enter the HTTP port number."
-
-PageInstallSetting() {
-  local page=$(cat << EOF
-{
-	"step_title": "$install_title",
-	"invalid_next_disabled_v2": true,
-	"items": [{
-    "type": "textfield",
-    "desc": "$hostname_desc",
-    "subitems": [{
-      "key": "pkgwizard_hostname",
-      "desc": "$hostname_label",
-      "defaultValue": "$HOSTNAME",
-      "validator": {
-        "allowBlank": false
-      }
-    }]
-  },{
-    "type": "textfield",
-    "desc": "$ssh_port_desc",
-    "subitems": [{
-      "key": "pkgwizard_ssh_port",
-      "desc": "$ssh_port_label",
-      "defaultValue": "$GITLAB_SHELL_SSH_PORT",
-      "validator": {
-        "allowBlank": false,
-        "regex": {
-          "expr": "/^[1-9]\\\\d{0,4}$/"
-        }
-      }
-    }]
-  },{
-		"type": "textfield",
-		"desc": "$http_port_desc",
-		"subitems": [{
-			"key": "pkgwizard_http_port",
-			"desc": "$http_port_label",
-			"defaultValue": "$GITLAB_HTTP_PORT",
-			"validator": {
-				"allowBlank": false,
-				"regex": {
-					"expr": "/^[1-9]\\\\d{0,4}$/"
-				}
-			}
-		}]
-	}]
-}
-EOF
-)
-	echo "$page"
-}
-
 PageAdvancedSettings() {
   local page=$(cat << EOF
 {
@@ -105,23 +45,19 @@ PageAdvancedSettings() {
 	"activeate": "{
 	  const summary = document.getElementById('pkgwizard-install-summary');
 	  if(summary) {
-	  	  const hostname = document.querySelector('[name=\"pkgwizard_hostname\"]').value;
-    	  const ssh_port = document.querySelector('[name=\"pkgwizard_ssh_port\"]').value;
-    	  const http_port = document.querySelector('[name=\"pkgwizard_http_port\"]').value;
-
     	  summary.innerHTML =
     	    'cd /var/packages/$PKG_NAME/scripts && &bsol;<br>' +
     	    'sudo sh gitlab install $PKG_NAME &bsol;<br>' +
     	    '--version=$GITLAB_IMAGE_VERSION &bsol;<br>' +
     	    '--share=$PKG_NAME &bsol;<br>' +
-    	    '--hostname='+hostname+' &bsol;<br>' +
-    	    '--port-ssh='+ssh_port+' &bsol;<br>' +
-    	    '--port-http='+http_port;
+    	    '--hostname=$HOSTNAME &bsol;<br>' +
+    	    '--port-ssh=$GITLAB_SHELL_SSH_PORT &bsol;<br>' +
+    	    '--port-http=$GITLAB_HTTP_PORT';
 	  }
 	}",
 	"items": [{
     "desc": "
-After the installation is complete, you need to <a href=\"https://kb.synology.com/DSM/tutorial/How_to_login_to_DSM_with_root_permission_via_SSH_Telnet\" target=\"_blank\">login on your synology over ssh</a> and execute this command with root privileges. Please modify the arguments to fit your needs.<br>
+This package installs a toolkit for GitLab. After the installation is complete, you need to <a href=\"https://kb.synology.com/DSM/tutorial/How_to_login_to_DSM_with_root_permission_via_SSH_Telnet\" target=\"_blank\">login on your synology over ssh</a> and execute this command with root privileges. Please modify the arguments to fit your needs.<br>
 <br>
 <div id=\"pkgwizard-install-summary\" style=\"user-select: text; cursor: initial; font-family: monospace;\">
   <br><br><br><br><br><br><br><br><br><br> <!-- reserve some space -->
@@ -184,7 +120,6 @@ main()
 	install_page=$(page_append "$install_page" "$memory_check_page")
 
   install_title="${install_title}";
-  install_page=$(page_append "$install_page" "$(PageInstallSetting)")
   install_page=$(page_append "$install_page" "$(PageAdvancedSettings)")
 
 	echo "[$install_page]" > "${SYNOPKG_TEMP_LOGFILE}"
